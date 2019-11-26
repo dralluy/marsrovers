b1ba692619154036a669a4d4432d19e1822aba3d
David Ralluy <david.ralluy@mango.com>
2019-11-26 14:38:22 +0100

    Refactor: Extract constant for heading

--- src/test/java/org/code/marsrover/RoverShould.java
+++ src/test/java/org/code/marsrover/RoverShould.java
@@ -9,13 +9,14 @@ import static org.hamcrest.core.Is.is;
 
 @RunWith(JUnit4.class)
 public class RoverShould {
+
+    private static final String HEADING_NORTH = "N";
+
     @Test
     public void have_a_position_represented_by_a_coordinate_and_a_heading() {
-
         Coordinate coordinate = new Coordinate(1, 3);
-        String heading = "N";
 
-        Rover rover = new Rover(coordinate, heading);
+        Rover rover = new Rover(coordinate, HEADING_NORTH);
 
         Assert.assertThat(rover.getPosition(), is("1 3 N"));
     }
