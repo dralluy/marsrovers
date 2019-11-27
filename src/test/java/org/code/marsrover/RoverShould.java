ed8678f409d7c8c8e85e92f747cbea25e4696c79
David Ralluy <david.ralluy@mango.com>
2019-11-27 09:47:25 +0100

    Refactor: Parametized turn left rover test

--- src/test/java/org/code/marsrover/RoverShould.java
+++ src/test/java/org/code/marsrover/RoverShould.java
@@ -1,14 +1,17 @@
 package org.code.marsrover;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.runners.Parameterized;
 
 import static org.code.marsrover.Heading.*;
 import static org.hamcrest.core.Is.is;
 
-@RunWith(JUnit4.class)
+@RunWith(JUnitParamsRunner.class)
 public class RoverShould {
 
     @Test
@@ -75,13 +78,20 @@ public class RoverShould {
     }
 
     @Test
-    public void change_heading_to_south_when_left_command_received_and_heading_west() {
+    @Parameters(method = "parametersForHeadingTest")
+    public void change_to_coorect_heading_left_command_received(Heading initial, Heading expected) {
         Coordinate coordinate = new Coordinate(1, 3);
-        Position position = new Position(coordinate, WEST);
+        Position position = new Position(coordinate, initial);
 
         Rover rover = new Rover(position);
         rover.turnLeft();
 
-        Assert.assertThat(rover.getPosition().getHeading(), is(SOUTH));
+        Assert.assertThat(rover.getPosition().getHeading(), is(expected));
+    }
+
+    private Object[] parametersForHeadingTest() {
+        return new Object[] {
+                new Object[] { WEST, SOUTH }
+        };
     }
 }
