c4c1201fa0fb5fbc629db6d40e234518fd62d580
David Ralluy <dralluy@gmail.com>
2019-11-27 21:38:42 +0100

    Green: Generate Plateau from nasa command (null if empty message)

--- src/main/java/org/code/marsrover/NasaCommandGenerator.java
+++ src/main/java/org/code/marsrover/NasaCommandGenerator.java
@@ -1,5 +1,9 @@
 package org.code.marsrover;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class NasaCommandGenerator {
     private final String nasaCommand;
 
@@ -7,12 +11,19 @@ public class NasaCommandGenerator {
         this.nasaCommand = nasaCommand;
     }
 
-    public Coordinate extractPlateauCoordinatesFromCommand() {
-        String[] coordinateSnippet = this.nasaCommand.split("\\s");
-        return new Coordinate(Integer.valueOf(coordinateSnippet[0]), Integer.valueOf(coordinateSnippet[1]));
+    public Optional<Coordinate> extractPlateauCoordinatesFromCommand() {
+        Pattern pattern = Pattern.compile("^(\\d\\s\\d)");
+        Matcher matcher = pattern.matcher(this.nasaCommand);
+
+        if (matcher.find()) {
+            String[] coordinateSnippet = matcher.group().split("\\s");
+            return Optional.of(new Coordinate(Integer.valueOf(coordinateSnippet[0]), Integer.valueOf(coordinateSnippet[1])));
+        }
+        return Optional.empty();
     }
 
     public Plateau getPlateau() {
-        return new Plateau(extractPlateauCoordinatesFromCommand());
+        Optional<Coordinate> coordinate = extractPlateauCoordinatesFromCommand();
+        return coordinate.isPresent()? new Plateau(coordinate.get()) : null;
     }
 }
