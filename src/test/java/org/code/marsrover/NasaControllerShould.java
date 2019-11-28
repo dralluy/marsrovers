76363b339e4f1ca80d6e54de62cfc8874a6a0319
David Ralluy <david.ralluy@mango.com>
2019-11-28 15:23:25 +0100

    Red: Controller should return end rovers position after executing nasa command

---
+++ src/test/java/org/code/marsrover/NasaControllerShould.java
@@ -0,0 +1,27 @@
+package org.code.marsrover;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnit4.class)
+public class NasaControllerShould {
+    @Test
+    public void get_rovers_end_position_when_executing_a_nasa_command() {
+        String nasaCommand = "5 5 1 2 N LMLMLMLMM 3 3 E MMRMMRMRRM";
+        String expectedEndRoversPosition = "1 3 N 5 1 E";
+
+        NasaInfrastructureCreator nasaInfrastructureCreator = new NasaInfrastructureCreator(nasaCommand);
+
+        String endRoversPosition = nasaInfrastructureCreator.getRovers().stream()
+                .map(rover -> rover.getPosition().toString())
+                .collect(Collectors.joining());
+        assertThat(endRoversPosition, is(expectedEndRoversPosition));
+    }
+}
