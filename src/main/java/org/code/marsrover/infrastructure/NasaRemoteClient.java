1e3472dbf0f89fc30ab6c7806b0619b78a360cc0
David Ralluy <dralluy@gmail.com>
2019-11-29 17:23:37 +0100

    Refactor remote client

--- src/main/java/org/code/marsrover/infrastructure/NasaRemoteClient.java
+++ src/main/java/org/code/marsrover/infrastructure/NasaRemoteClient.java
@@ -1,41 +1,61 @@
 package org.code.marsrover.infrastructure;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NasaRemoteClient {
 
+    private static Logger logger = Logger.getLogger("org.code.marsrover.infrastructure");
+
+    private static final String URL_HOST = "http://localhost:8000";
+    private static final String CONTEXT = "/mars";
+    private static final String QUERY_PARAM = "?command=";
+    private static final String HTTP_SPACE_CODE = "%20";
+    private static final String SPACE = "\\s";
+
     public String executeCommand(String nasaCommand) {
         try {
-            String formattedCommand = nasaCommand.replaceAll("\\s", "%20");
-            String urlString = "http://localhost:8000/mars?command="+formattedCommand;
-            URL url = new URL(urlString);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoOutput(true);
+            String formattedCommand = nasaCommand.replaceAll(SPACE, HTTP_SPACE_CODE);
+            URL url = new URL(URL_HOST + CONTEXT + QUERY_PARAM + formattedCommand);
 
-            int status = connection.getResponseCode();
+            HttpURLConnection connection = makeConnection(url);
 
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
+            StringBuilder content = extractResponseFrom(connection);
 
-            connection.disconnect();
+            closeConnection(connection);
 
             return content.toString();
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Server error");
         }
         return null;
+    }
+
+    private void closeConnection(HttpURLConnection connection) {
+        connection.disconnect();
+    }
+
+    private StringBuilder extractResponseFrom(HttpURLConnection connection) throws IOException {
+        StringBuilder content = new StringBuilder();
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        while ((inputLine = inputReader.readLine()) != null) {
+            content.append(inputLine);
+        }
+        inputReader.close();
+        return content;
+    }
+
+    private HttpURLConnection makeConnection(URL url) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+        return connection;
     }
 }
